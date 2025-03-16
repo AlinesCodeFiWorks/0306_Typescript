eff3ecae78a7202ce265404bb0148f6548a70fc3
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-03-15 23:48:16 -0500

    Practicing typescript

--- typescript.ts
+++ typescript.ts
@@ -18,6 +18,23 @@ function logBookDetails(book: Book): void {
 
 logBookDetails(book1);
 //Exercise 2: Working with Typed Functions
+function calculateArea(width: number, height: number): number {
+  return width * height;
+}
+let area = calculateArea(5, 10);
+console.log(area);
 //Exercise 3: Using Arrays with TypeScript
+type Movie = { title: string; year: number };
+let favoriteMovies: Movie[] = [
+  { title: "Inception", year: 2010 },
+  { title: "The Matrix", year: 1999 },
+];
+function addMovie(newMovie: Movie): void {
+  favoriteMovies.push(newMovie);
+}
+addMovie({ title: "Interstellar", year: 2014 });
+favoriteMovies.forEach((movie) => {
+  console.log(`${movie.title} (${movie.year})`);
+});
 //Exercise 4: Understanding Union Types
 //Exercise 5: Exploring Generics

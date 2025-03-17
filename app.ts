2aa03600a1bfc223164d04b01076ead1ad2f0720
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-03-16 23:59:30 -0500

    Worked on ts exercises some more - updating main, not done

--- app.ts
+++ app.ts
@@ -1,5 +1,22 @@
 //Post-class exercises
 //Exercise 1: Building a Simple User Management System
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+let users: User[] = [
+  User1 { id: 1, name: "Alice", email: "alice@example.com" },
+  { id: 2, name: "Bob", email: "bob@example.com" },
+];
+
+function findUserById(ID: number): void {
+  users.find((ID)=>ID===users.id);
+}
+
+console.log(findUserById(1));
+
 //Exercise 2: Validating Input with Type Guards
 //Exercise 3: Exploring Enums
 //Exercise 4: Leveraging Type Aliases
